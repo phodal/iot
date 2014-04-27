1340885c454c5057ae1c077943ebb3107cadfe1f
gmszone <gmszone@qq.com>
2014-04-27 12:06:16 +0800

    [Python] Phodal: fix the scripts of post

--- python/post.py
+++ python/post.py
@@ -1,9 +1,7 @@
-import json
+#!/usr/bin/python
 import requests
 
 url = "http://b.phodal.com/athome/1"
-
-data = {"temperature": 19, "sensors1": 22, "sensors2": 7.5, "led1": 1, "method": "PUT"}
-
+data = {"temperature": 19, "sensors1": 32, "sensors2": 7.5, "led1": 0, "method": "PUT"}
 r = requests.put(url, data)
 print r.text
