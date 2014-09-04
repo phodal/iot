f8bf4c947144c0bd1deed71290efb8b92fb975a9
Phodal Huang <fengda.huang@rea-group.com>
2014-09-04 16:59:47 +0800

    add the python get

--- python/get.py
+++ python/get.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python
-import json
-import urllib2
-import serial
-import time
+import requests
 
-url="http://b.phodal.com/athome/1"
-
-while 1:
-    try:
-        date=urllib2.urlopen(url)
-        result=json.load(date)
-        status=result[0]['led1']
-        print result
-    except urllib2.URLError:
-        print "Bad URL or timeout"
-
+url = "http://b.phodal.com/athome/1"
+r = requests.get(url)
+print r.text
